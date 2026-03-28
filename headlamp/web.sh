234df1dbb59de7f9c50b004bcd7829b7cf504ecd
kvtodev <kvtodev@outlook.com>
2026-03-28 12:21:16 +0800

    update

--- headlamp/web.sh
+++ headlamp/web.sh
@@ -1,3 +1,3 @@
 kubectl create token headlamp --namespace headlamp
 echo "https://localhost:8081"
- kubectl port-forward -n headlamp service/headlamp 8081:80
+kubectl port-forward -n headlamp service/headlamp 8081:80
